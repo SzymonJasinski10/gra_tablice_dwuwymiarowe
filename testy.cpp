7018a05fc2d638e294ba0215c2ec5c9b37fa8043
Szymon Jasinski <jasinski.szymon1@wp.pl>
2026-05-31 19:31:45 +0200

    Poprawiono blad ze srednikiem i przetestowano program

--- testy.cpp
+++ testy.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
-int main();
+int main()
 {
-cout<<"test, raz dwa trzy, wszystko działa?";
-return 0;
+	cout<<"test, raz dwa trzy, wszystko działa?";
+	return 0;
 }
