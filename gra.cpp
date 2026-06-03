066e0aabc632a0a50a16379f932a63aa6a0501a5
SzymonJasinski10 <jasinski.szymon1@wp.pl>
2026-06-03 08:15:03 +0200

    Ustalenie celu gry i uaktualnienie plikow z wczoraj

---
+++ gra.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+cout<<"============================================"<<endl;
+cout<<" <<WITAJ W GRZE - LABIRYTN ZE SKARBAMI!!!>> "<<endl;
+cout<<"      || AUTOR: SZYMON JASINSKI 1D || "<<endl;
+cout<<"============================================"<<endl;
+
+}
