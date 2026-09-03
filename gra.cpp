d7b176bf1d210361cc94fc2e02a8380973044f52
Szymon Jasinski <jasinski.szymon1@wp.pl>
2026-09-03 21:22:07 +0200

    Ulepszenie gry, dodanie instrukcji, oficjalne testy

--- gra.cpp
+++ gra.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <conio.h>
+#include <cstdlib>
 using namespace std;
 int main(){
 
@@ -8,7 +9,16 @@ cout<<" <<WITAJ W GRZE - LABIRYTN ZE SKARBAMI!!!>> "<<endl;
 cout<<"      || AUTOR: SZYMON JASINSKI 1D || "<<endl;
 cout<<"============================================"<<endl;
 cout<<endl;
+cout<<"INSTRUKCJA GRY"<<endl;
+cout<<"1; Gracz to cyfra 1 porusza sie uzywajac przyciskow WASD."<<endl;
+cout<<"2; Aby wygrac gracz musi zebrac 5 skarbow oznaczine na planszy cyframi 5, nie wchodzac przy tym w zadna sciane."<<endl;
+cout<<"3; Wejscie w sciane oznaczona na plaszy cyfra 0 bedzie skutkowalo przegrana."<<endl;
+cout<<"4; Podczas rozgrywki gracz moze nacisnac x, aby wyjsc z gry."<<endl;
+cout<<"5; Aby rozpoczac gre nalezy nacisnac dowolny przycisk na klawiaturze."<<endl;
 cout<<endl;
+cout<<endl;
+
+_getch();
 
 int rozmiar=10;
 int puste_pole=' ';
@@ -17,7 +27,7 @@ int plansza[10][10] = {
 {0,1,' ',' ',0,' ',' ',' ',5,0},
 {0,' ',0,' ',0,' ',0,0,' ',0},
 {0,5,0,' ',' ',' ',' ',0,' ',0},
-{0,0,0,' ',0,' ',0,' ',0},
+{0,0,0,' ',0,' ',0,' ',0,0},
 {0,' ',' ',' ',0,' ',' ',' ',' ',0},
 {0,' ',0,' ',0,0,0,0,' ',0},
 {0,' ',0,' ',' ',' ',5,0,' ',0},
@@ -31,13 +41,22 @@ int graczY=1;
 int maks_skarbow=5;
 int liczba_skarbow=0;
 int stanGry=0; //0-gramy, 1-wygrana, 2-przegrana 3-wyjscie z gry
-
+int licznik_komunikatu=0;
 while(stanGry==0){
+        system("cls");
+
+        if(licznik_komunikatu == 1){
+    cout<<"<<<<<<<<<<<<<< Wow, gratulacje znalazles skarb >>>>>>>>>>>>>>"<<endl;
+    licznik_komunikatu=0;
+
+}
+
+
 if(liczba_skarbow==0){
     cout<<"---------------------------------------------------------------"<<endl;
 cout<<"                    Gramy dalej..."<<endl;
 cout<<"                 Zebrane skarby: "<<liczba_skarbow<<"/5"<<endl;
-cout<<"Aby wygrac do zebrania zostalo ci jeszcze "<<5-liczba_skarbow<<" skarbów do zebrania"<<endl;
+cout<<"Aby wygrac do zebrania zostalo ci jeszcze "<<5-liczba_skarbow<<" skarbow do zebrania"<<endl;
 cout<<"---------------------------------------------------------------"<<endl;
 cout<<endl;
 }
@@ -78,31 +97,8 @@ cout<<endl;
     }
        cout<<endl;
   }
-  if(stanGry==0){
-  cout<<endl;
-  cout<<endl;
-  cout<<endl;
-  cout<<endl;
-  cout<<endl;
-  cout<<endl;
-  cout<<endl;
-  }
-  else{
-   cout<<endl;
-  cout<<endl;
-  cout<<endl;
-  cout<<endl;
-  cout<<endl;
-  cout<<endl;
-  cout<<endl;
-  cout<<endl;
-  cout<<endl;
-  cout<<endl;
-  cout<<endl;
-  cout<<endl;
-  cout<<endl;
-  cout<<endl;
-  }
+
+
 
   char klawisz=_getch();
 
@@ -124,13 +120,12 @@ cout<<endl;
   else{
     if(plansza[ruchowaX][ruchowaY]==5){
         liczba_skarbow++;
+        licznik_komunikatu = 1;
         if(liczba_skarbow==maks_skarbow){
         stanGry=1;
 
     }
-    if(liczba_skarbow<maks_skarbow-1){
-        cout<<"<<<<<<<<<<<<<< Wow, gratulacje znalazles skarb >>>>>>>>>>>>>>"<<endl;
-    }
+
 
     }
     plansza[graczX][graczY] = ' ';
